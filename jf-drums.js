597c77c485e38ee5a9c279630b3d4acbc25b3447
Jael1997 <je_tauro55@hotmail.com>
2024-03-12 21:25:08 -0500

    Initial commit

---
+++ jf-drums.js
@@ -0,0 +1,48 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Jafet";
+
+//var nombre1 = "Jafet";
+
+//const nombre2 = "Jafet";
+
+//console.log (nombre);
+//console.log (nombre1);
+//console.log (nombre2);
+
+//SELECCIONAR ELEMENTOS
+
+/*let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector(".acerca-de");
+
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if (textoTitulo == "Nombre") {
+    titulo.innerHTML = "Otro";
+}
+
+else {
+    console.log ("No se cumple");
+}
+
+//FUNCIONES
+
+let nombre = "Jafet";
+let ciudad = "Lima";
+let gusto = "chocolate";
+
+let parrafo = document.querySelector(".parrafo2");
+
+function cambiarTexto (nombre, ciudad, gusto) {
+    let contenido = 'Me llamo ${nombre}, nací en ${ciudad} y vivo en Ventanilla. Me gusta la ${gusto} y ver películas. Me encataría aprender a programar.';
+
+    return contenido;
+}
+
+parrafo.innerText("Elias", ciudad, gusto);*/
